1cc4e669b09c9054820ba53cb2020e0b4c73c912
BuildTools <unconfigured@null.spigotmc.org>
2025-02-20 18:21:35 +0200

    deplyment test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./lib/queryClient";
-import { SEO } from "./features/seo/seo.component";
+import { SEO } from "./features/seo/Seo.component";
 import { HelmetProvider } from "react-helmet-async";
 
 function App() {
